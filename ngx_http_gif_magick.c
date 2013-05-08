f362d2bc2cac1b3cb941fba20f0f385084412998
Michael Schenck <michael@tumblr.com>
2013-05-08 12:23:33 -0400

    Filling in the handler section and prepping for some Magick ;)

--- ngx_http_gif_magick.c
+++ ngx_http_gif_magick.c
@@ -7,6 +7,10 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
+#include <wand/MagickWand.h>
+
+#define MAX_IMAGE_SIZE 20971520  /* 20 MB */
+
 typedef struct {
   ngx_uint_t  width;
   ngx_uint_t  height;
@@ -89,6 +93,27 @@ ngx_http_gif_magick_merge_loc_conf( ngx_conf_t *cf, void *parent, void *child ) 
 
 static ngx_int_t
 ngx_http_gif_magick_handler  ( ngx_http_request_t *request ) {
-  ngx_http_gif_magick_loc_conf_t *gif_magick_conf;
+  ngx_http_gif_magick_loc_conf_t  *gif_magick_conf;
+  ngx_buf_t                       *buffer;
+  ngx_chain_t                     out_chain;
+
+  // Load gif_magick config
   gif_magick_conf = ngx_http_get_module_loc_conf( request, ngx_http_gif_magick_module );
+
+  // Headers
+  request->headers_out.status = NGX_HTTP_OK;
+  ngx_http_send_header( request );
+
+  // Body
+  buffer = ngx_pcalloc( request->pool, sizeof( ngx_buf_t ) );
+  if ( buffer == NULL ) {
+    ngx_log_error( NGX_LOG_ERR, request->connection->log, 0, "Failed to allocate response buffer.");
+    return NGX_HTTP_INTERNAL_SERVER_ERROR;
+  }
+
+  out_chain.buf = buffer;
+  out_chain.next = NULL;
+
+  return ngx_http_output_filter( request, &out_chain );
 }
+
