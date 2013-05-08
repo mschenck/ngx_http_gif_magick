19f89b6c6d8b181a4c10274deaf1a818ba89548e
Michael Schenck <michael@tumblr.com>
2013-05-07 22:00:36 -0400

    initial check-in

---
+++ ngx_http_gif_magick.c
@@ -0,0 +1,10 @@
+/******************************************************************************\
+* ngx_http_gif_magick
+*
+\******************************************************************************/
+
+typedef struct {
+  ngx_uint_t  methods;
+  ngx_flag_t  create_full_put_path;
+  ng
+} ngx_http_gif_magick_loc_conf_t;
